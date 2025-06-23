e0299289e032dbf103014e044cef0b8d2e3478ea
Vasu Pandey <pandeyvasu0694@gmail.com>
2025-06-23 11:46:15 +0530

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,175 @@
+import logo from './food.webp'
+import logo_dark from './logo_dark.svg'
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import header_img from './header_img.webp'
+import brand_img from './brand_img.webp'
+import project_img_1 from './project_img_1.webp'
+import project_img_2 from './project_img_2.webp'
+import project_img_3 from './project_img_3.webp'
+import project_img_4 from './project_img_4.webp'
+import project_img_5 from './project_img_5.webp'
+import project_img_6 from './project_img_6.webp'
+import project_img_7 from './project_img_7.webp'
+import project_img_8 from './project_img_8.webp'
+import project_img_9 from './project_img_9.webp'
+import project_img_10 from './project_img_10.webp'
+import project_img_11 from './project_img_11.webp'
+import project_img_12 from './project_img_12.webp'
+import bg_image from './bg_image.webp'
+import profile_img_1 from './profile_img_1.webp'
+import profile_img_2 from './profile_img_2.webp'
+import profile_img_3 from './profile_img_3.webp'
+import back_img from './back_img.avif'
+import back_img_2 from './back_img_2.webp'
+import brand_img_1 from './brand_img_1.avif'
+import brand_img_2 from './brand_img_2.png'
+import brand_img_3 from './brand_img_3.avif'
+import brand_img_4 from './brand_img_4.png'
+import brand_img_5 from './brand_img_5.png'
+import brand_img_6 from './brand_img_6.jpg'
+import brand_img_7 from './brand_img_7.png'
+import brand_img_8 from './brand_img_8.jpeg'
+import brand_img_9 from './brand_img_9.png'
+
+export const assets = {
+    logo,
+    logo_dark,
+    cross_icon,
+    menu_icon,
+    star_icon,
+    header_img,
+    brand_img,
+    project_img_1,
+    project_img_2,
+    project_img_3,
+    project_img_4,
+    project_img_5,
+    project_img_6,
+    project_img_7,
+    project_img_8,
+    project_img_9,
+    project_img_10,
+    project_img_11,
+    project_img_12,
+    brand_img_1,
+    brand_img_2,
+    brand_img_3,
+    brand_img_4,
+    brand_img_5,
+    brand_img_6,
+    brand_img_7,
+    brand_img_8,
+    brand_img_9,
+    back_img,
+    back_img_2,
+    bg_image,
+    left_arrow,
+    right_arrow,
+}
+
+export const projectsData = [
+    {
+      title: "Food 1",
+      price: "$2,50,000",
+      location: "California The golden sun dipped below the horizon, casting warm hues across the quiet landscape. Birds chirped softly in the distance while a gentle breeze rustled the leaves. Children’s laughter echoed faintly from a nearby park. Time slowed down, offering a rare moment of peace in a world constantly moving forward.",
+      image: project_img_1
+    },
+    {
+      title: "Food 2",
+      price: "$2,50,000",
+      location: "San Francisco The golden sun dipped below the horizon, casting warm hues across the quiet landscape. Birds chirped softly in the distance while a gentle breeze rustled the leaves. Children’s laughter echoed faintly from a nearby park. Time slowed down, offering a rare moment of peace in a world constantly moving forward.",
+      image: project_img_2
+    },
+    {
+      title: "Food 3",
+      price: "$2,50,000",
+      location: "Chicago The golden sun dipped below the horizon, casting warm hues across the quiet landscape. Birds chirped softly in the distance while a gentle breeze rustled the leaves. Children’s laughter echoed faintly from a nearby park. Time slowed down, offering a rare moment of peace in a world constantly moving forward.",
+      image: project_img_3
+    },
+    {
+      title: "Food 4",
+      price: "$2,50,000",
+      location: "Los Angeles The golden sun dipped below the horizon, casting warm hues across the quiet landscape. Birds chirped softly in the distance while a gentle breeze rustled the leaves. Children’s laughter echoed faintly from a nearby park. Time slowed down, offering a rare moment of peace in a world constantly moving forward.",
+      image: project_img_4
+    },
+    {
+      title: "Food 5",
+      price: "$2,50,000",
+      location: "San Francisco The golden sun dipped below the horizon, casting warm hues across the quiet landscape. Birds chirped softly in the distance while a gentle breeze rustled the leaves. Children’s laughter echoed faintly from a nearby park. Time slowed down, offering a rare moment of peace in a world constantly moving forward.",
+      image: project_img_5
+    },
+    {
+      title: "Food 6",
+      price: "$2,50,000",
+      location: "Chicago The golden sun dipped below the horizon, casting warm hues across the quiet landscape. Birds chirped softly in the distance while a gentle breeze rustled the leaves. Children’s laughter echoed faintly from a nearby park. Time slowed down, offering a rare moment of peace in a world constantly moving forward.",
+      image: project_img_6
+    },
+    {
+      title: "Food 7",
+      price: "$2,50,000",
+      location: "Chicago The golden sun dipped below the horizon, casting warm hues across the quiet landscape. Birds chirped softly in the distance while a gentle breeze rustled the leaves. Children’s laughter echoed faintly from a nearby park. Time slowed down, offering a rare moment of peace in a world constantly moving forward.",
+      image: project_img_7
+    },
+    {
+      title: "Food 8",
+      price: "$2,50,000",
+      location: "Chicago The golden sun dipped below the horizon, casting warm hues across the quiet landscape. Birds chirped softly in the distance while a gentle breeze rustled the leaves. Children’s laughter echoed faintly from a nearby park. Time slowed down, offering a rare moment of peace in a world constantly moving forward.",
+      image: project_img_8
+    },
+    {
+      title: "Food 9",
+      price: "$2,50,000",
+      location: "Chicago The golden sun dipped below the horizon, casting warm hues across the quiet landscape. Birds chirped softly in the distance while a gentle breeze rustled the leaves. Children’s laughter echoed faintly from a nearby park. Time slowed down, offering a rare moment of peace in a world constantly moving forward.",
+      image: project_img_9
+    },
+    {
+      title: "Food 10",
+      price: "$2,50,000",
+      location: "Chicago The golden sun dipped below the horizon, casting warm hues across the quiet landscape. Birds chirped softly in the distance while a gentle breeze rustled the leaves. Children’s laughter echoed faintly from a nearby park. Time slowed down, offering a rare moment of peace in a world constantly moving forward.",
+      image: project_img_10
+    },
+    {
+      title: "Food 11",
+      price: "$2,50,000",
+      location: "Chicago The golden sun dipped below the horizon, casting warm hues across the quiet landscape. Birds chirped softly in the distance while a gentle breeze rustled the leaves. Children’s laughter echoed faintly from a nearby park. Time slowed down, offering a rare moment of peace in a world constantly moving forward.",
+      image: project_img_11
+    },
+    {
+      title: "Food 12",
+      price: "$2,50,000 The golden sun dipped below the horizon, casting warm hues across the quiet landscape. Birds chirped softly in the distance while a gentle breeze rustled the leaves. Children’s laughter echoed faintly from a nearby park. Time slowed down, offering a rare moment of peace in a world constantly moving forward.",
+      location: "Chicago",
+      image: project_img_12
+    },
+    
+  ];
+
+  export const testimonialsData = [
+    {
+        name: "Donald Jackman",
+        title: "Marketing Manager",
+        image: profile_img_1,
+        alt: "Portrait of Donald Jackman",
+        rating: 5,
+        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+    },
+    {
+        name: "Richard Nelson",
+        title: "UI/UX Designer",
+        image: profile_img_2,
+        alt: "Portrait of Richard Nelson",
+        rating: 4,
+        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+    },
+    {
+        name: "James Washington",
+        title: "Co-Founder",
+        image: profile_img_3,
+        alt: "Portrait of James Washington",
+        rating: 5,
+        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+    }
+];
